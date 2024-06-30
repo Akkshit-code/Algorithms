0b820bae5e44580875dbc1ee0f9ec50a9166562f
Akkshit-code <akkshitsingh27@gmail.com>
2024-06-30 20:59:43 +0530

    Debug(Useless)

--- SieveOfErasthonese.java
+++ SieveOfErasthonese.java
@@ -1,25 +1,37 @@
 import java.util.Arrays;
 
 public class SieveOfErasthonese {
-    public static void seive(int n){
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
-        for (int i=2;i*i<=n;i++){
-            if(isPrime[i]){
-                for (int j=2*i;j<=n;j+=i){
-                    isPrime[j]=false;
+        public static int countPrimes(int n) {
+            if (n <= 2) {
+                return 0;
+            }
+
+            boolean[] isPrime = new boolean[n];
+            Arrays.fill(isPrime, true);
+            isPrime[0] = false;
+            isPrime[1] = false;
+            for (int i = 2; i * i < n; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = false;
+                    }
                 }
             }
-        }
-        for (int i=2;i<=n;i++) {
-            if (isPrime[i]) {
-                System.out.print(" "+i);
+
+            int count = 0;
+            for (int i = 2; i < n; i++) {
+                if (isPrime[i]) {
+                    count++;
+                }
             }
+
+            return count;
         }
-    }
+
+
 
     public static void main(String[] args) {
-        int n=20;
-        seive(n);
+        int n=3;
+        System.out.println(countPrimes(n));
     }
 }
